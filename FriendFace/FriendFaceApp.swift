74aa91e7b156978ded088e969a65514422f0e1b0
TheGoodAres <61661335+TheGoodAres@users.noreply.github.com>
2023-04-16 11:18:03 +0100

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Robert-Dumitru Oprea on 16/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
